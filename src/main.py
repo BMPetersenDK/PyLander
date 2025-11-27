2e5cf5e3ebcf96c5faafd9e8c659a31c9848dd9a
Bo <kemibo@gmail.com>
2025-11-27 21:43:07 +0100

    Minimal pygame setup addedd

--- src/main.py
+++ src/main.py
@@ -1,3 +1,28 @@
+"""
+Main entry point of the game.
+"""
+from settings import * #pylint: disable=wildcard-import
+
+class Game:
+    """
+    Main game class. 
+    """
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+
+    def run(self):
+        """
+        run Run the game
+        """
+        quitting = False
+        while quitting is not True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quitting = True
+            pygame.display.update()
+        pygame.quit()
 
 if __name__ == '__main__':
-    print('Hello World')
+    game = Game()
+    game.run()
---
+++ src/settings.py
@@ -0,0 +1,7 @@
+"""
+    _summary_
+     This module contains all global settings used in the game.
+"""
+import pygame #pylint: disable=unused-import
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
