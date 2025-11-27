bcad690cd3d860ab6472a6ad7c7c1e8930423299
Bo <kemibo@gmail.com>
2025-11-27 22:19:21 +0100

    Added logging using basicConfig()

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 """
 Main entry point of the game.
 """
+import logging
+
 from settings import * #pylint: disable=wildcard-import
+
+
 
 class Game:
     """
@@ -15,14 +19,26 @@ class Game:
         """
         run Run the game
         """
+        logging.info("Starting Game.run()")
         quitting = False
         while quitting is not True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     quitting = True
+                    logging.info("Pygame QUIT event captured. quitting.")
             pygame.display.update()
         pygame.quit()
 
+
 if __name__ == '__main__':
+    logging.basicConfig(
+        filename="logs/app.log",
+        encoding="utf-8",
+        filemode="w",
+        format="{asctime} - {levelname} - {message}",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.INFO
+        )
     game = Game()
     game.run()
